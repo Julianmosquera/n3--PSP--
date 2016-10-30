6f240eec95a74daeb9c7125318536248a9f4a84a
Julian Mosquera <julianmosqueraguimerans@gmail.com>
2016-10-30 10:44:52 +0100

    con el join dentro terminado

--- src/n3/psp/Fio.java
+++ src/n3/psp/Fio.java
@@ -1,29 +1,46 @@
 package n3.psp;
 
-
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Julián
  */
 public class Fio extends Thread {
-    
+
     public Fio(String name) {
         super(name);
     }
 
-   
-    
     @Override
     public void run() {
-        
+
         System.out.println("Iniciando :" + getName());
-        for (int i = 0; i <=5; i++) {
+        for (int i = 0; i <= 5; i++) {
             Thread h1 = new Thread();
-            h1.setName("p1_"+i);
-            System.out.println(h1.getName()+"Procesando");
-           
+            
+            h1.start();
+            h1.setName("Principal");
+            System.out.println(h1.getName()+" Iniciando "+i);
+            for (int j = 0; j <= 10; j++) {
+                Thread sub=new Thread();
+                sub.setName("subhilo  "+j);
+                sub.start();
+                System.out.println(sub.getName() + " Procesando...");
+            }
+
+            h1.setName("Principal" + i);
+            
+            try {
+                h1.join();
+            } catch (InterruptedException ex) {
+                Logger.getLogger(Fio.class.getName()).log(Level.SEVERE, null, ex);
+            }
         }
-        System.out.println(getName()+" Terminó \n");
-    }
-}
+        System.out.println(getName() + " Terminó \n");
+        
+       
+        
+                 }}
+    
--- src/n3/psp/N3PSP.java
+++ src/n3/psp/N3PSP.java
@@ -10,27 +10,7 @@ public class N3PSP {
     
     public static void main(String[] args) throws InterruptedException {
         Fio f1= new Fio("f1");
-        f1.setName("Hilo 1");
-         Fio f2= new Fio("f2");
-        f2.setName("Hilo 2");
-         Fio f3= new Fio("f3");
-        f3.setName("Hilo 3");
-         Fio f4= new Fio("f4");
-        f4.setName("Hilo 4");
-         Fio f5= new Fio("f5");
-        f5.setName("Hilo 5");
         f1.start();
-        f1.join();
-        f2.start();
-        f2.join();
-        f3.start();
-        f3.join();
-        f4.start();
-        f4.join();
-        f5.start();
-        f5.join();
-        
-        
     }
     
 }
